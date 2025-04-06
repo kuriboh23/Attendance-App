5ba791548c4047c13e975e2d7ad88f6709539dda
Hamza <osathenas23@gmail.com>
2025-04-06 17:02:53 +0100

    commit

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -8,10 +10,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var guest_access:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
+        guest_access = findViewById(R.id.guest_access)
+
+        guest_access.setOnClickListener {
+            val intent = Intent(this,GuestActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
