8c76080b44b25860e8fa0377f1a0523a8ab9f50e
Ayanokouji <osathenas23@gmail.com>
2025-04-07 18:00:00 +0100

    Home Layout

--- app/src/main/java/com/example/project/signIn.kt
+++ app/src/main/java/com/example/project/signIn.kt
@@ -1,19 +1,28 @@
 package com.example.project
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SignIn:AppCompatActivity() {
     lateinit var back:ImageView
+    lateinit var login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         back = findViewById(R.id.arrowLeft)
+        login = findViewById(R.id.signIn_btn)
 
         back.setOnClickListener {
             finish()
+        }
+        login.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
