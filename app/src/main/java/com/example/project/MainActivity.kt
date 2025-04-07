b5fe6224aef9e790227d2dac017748829b88b1ec
Hamza <osathenas23@gmail.com>
2025-04-07 09:41:21 +0100

    New signUp layout with some changes

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.project
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     lateinit var guest_access:TextView
+    lateinit var createAcountBtn:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         guest_access = findViewById(R.id.guest_access)
+        createAcountBtn= findViewById(R.id.createAccount_btn)
 
         guest_access.setOnClickListener {
             val intent = Intent(this,GuestActivity::class.java)
+            startActivity(intent)
+        }
+        createAcountBtn.setOnClickListener {
+            val intent = Intent(this,SignUp::class.java)
             startActivity(intent)
         }
     }
