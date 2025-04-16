aa15d6febfcff37bfb2c88f5f88938fcc15d22de
Ayanokouji <osathenas23@gmail.com>
2025-04-16 13:43:40 +0100

    Apply Leave

---
+++ app/src/main/java/com/example/project/ApplyLeave.kt
@@ -0,0 +1,12 @@
+package com.example.project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ApplyLeave: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_apply_leave)
+    }
+}
--- app/src/main/java/com/example/project/fragment/Leave.kt
+++ app/src/main/java/com/example/project/fragment/Leave.kt
@@ -1,22 +1,32 @@
 package com.example.project.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.project.ApplyLeave
 import com.example.project.R
+import com.example.project.databinding.FragmentLeaveBinding
 
 
 class Leave : Fragment() {
-
+    lateinit var binding: FragmentLeaveBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leave, container, false)
+        binding = FragmentLeaveBinding.inflate(inflater, container, false)
+
+        binding.addRequest.setOnClickListener {
+            val intent = Intent(requireContext(), ApplyLeave::class.java)
+            startActivity(intent)
+        }
+
+        return binding.root
     }
 
 }
