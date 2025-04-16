d905ad754ce35cc2fd29ecd969a2e5fedd518360
Hamza <osathenas23@gmail.com>
2025-04-16 20:41:38 +0100

    Apply Leave

--- app/src/main/java/com/example/project/ApplyLeave.kt
+++ app/src/main/java/com/example/project/ApplyLeave.kt
@@ -2,11 +2,38 @@ package com.example.project
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.project.databinding.ActivityApplyLeaveBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ApplyLeave: AppCompatActivity() {
+    lateinit var binding: ActivityApplyLeaveBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_apply_leave)
+        binding = ActivityApplyLeaveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select a Date")
+            .build()
+
+        binding.dateEditText.setOnClickListener {
+            datePicker.show(this.supportFragmentManager, "DATE_PICKER")
+        }
+
+        datePicker.addOnPositiveButtonClickListener { selectedDate ->
+            val formattedDate = SimpleDateFormat("dd MMM yyyy", Locale.getDefault()).format(Date(selectedDate))
+            binding.dateEditText.setText(formattedDate)
+        }
+
+        binding.tvbackArrow.setOnClickListener {
+            finish()
+
+        }
+
     }
 }
